8aa89692e77d10ed513717f95c9190460294817d
Prathamesh0105 <149521210+Prathamesh0105@users.noreply.github.com>
2025-07-11 07:35:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Trail of ones/trail-of-ones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int countConsec(int n) {
+        // code here
+        int one = 1;
+        int zero = 1;
+        for(int i = 1; i < n; i++) {
+            zero += one;
+            one = zero - one;
+        }
+        
+        return pow(2, n) - (zero + one);
+    }
+};
