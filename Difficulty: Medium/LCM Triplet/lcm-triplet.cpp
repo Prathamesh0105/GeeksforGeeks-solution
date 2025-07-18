1dd1cffc3c11e9e40a86f35d922a11f8a0e4c90e
Prathamesh0105 <149521210+Prathamesh0105@users.noreply.github.com>
2025-07-18 08:40:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/LCM Triplet/lcm-triplet.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int lcmTriplets(int n) {
+        if(n<3)return n;
+        
+        if(n%2)return n*(n-1)*(n-2);
+        
+
+        if(n%3 == 0)return (n-1)*(n-2)*(n-3);
+        
+        return n*(n-1)*(n-3);
+    }
+};
+ 
