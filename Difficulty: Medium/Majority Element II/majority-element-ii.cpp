d85655c1661beb6910939108a7dad1e7f3cf8379
Prathamesh0105 <149521210+Prathamesh0105@users.noreply.github.com>
2025-07-26 11:11:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element II/majority-element-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    vector<int> findMajority(vector<int>& arr) {
+        unordered_map<int,int>hash;
+        for(auto i:arr)hash[i]++;
+        int n=arr.size();
+        vector<int>ans;
+        int k=(n)/3;
+        for(auto i:hash){
+            if(i.second>k)ans.push_back(i.first);
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
